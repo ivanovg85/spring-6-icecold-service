a7abffa4cbd6846c2c7cb7b00fd94926f6ceb09b
Georgi Ivanov <georgi.ivanov85@hotmail.com>
2025-11-28 16:42:46 +0200

    initial commit

---
+++ src/main/java/art/cookedincode/spring6icecoldservice/Spring6IcecoldServiceApplication.java
@@ -0,0 +1,13 @@
+package art.cookedincode.spring6icecoldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6IcecoldServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6IcecoldServiceApplication.class, args);
+    }
+
+}
